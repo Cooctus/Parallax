f924da1dbf5fb000785bc6b181e46e353d1b7ece
Brandon <420keklord@gmail.com>
2018-11-25 16:07:48 -0600

    Add files via upload

--- src/main.rs
+++ src/main.rs
@@ -5,7 +5,10 @@ use std::fs;
 extern crate clap;
 use clap::*;
 use std::path::Path;
+use std::ptr::null;
+use std::collections::HashMap;
 use std::io;
+use std::fs::OpenOptions;
 extern crate regex;
 use std::process::*;
 use regex::Regex;
@@ -14,30 +17,41 @@ use std::env::*;
 extern crate reqwest;
 #[macro_use]
 extern crate duct;
+mod repo;
+mod checkers;
 struct Package{
     build_instructions:  String,
     url:  String,
     name: String,
-    file_name: String
+    file_name: String,
+    version: String
 }
 
 impl Package {
 
     fn set_vars(&mut self, file_contents: &String) {
         let lines: Vec<&str> = file_contents.split('\n').filter(|&s| s != "").collect();
-        let url_regex = Regex::new(r"^URL\s=\s\W(\S+)\W").unwrap();
-        let build_regex = Regex::new(r"^BUILD_INSTRUCTIONS\s=\s*(.+)").unwrap();
-        let file_name_regex = Regex::new(r"^NAME\s=\s*(.+)").unwrap();
+        let url_regex = Regex::new(r"^URL\s=\s\W(\S+)\W*").unwrap();
+        let build_regex = Regex::new(r"^BUILD_INSTRUCTIONS\s=\s*\W*(\S+)\W*").unwrap();
+        let version_regex = Regex::new(r"^VERSION\s=\s*\W*(\S+)\W*").unwrap();
+        let file_name_regex = Regex::new(r"^NAME\s=\s*\W*(\S+)\W*").unwrap();
         for i in lines.clone() {
             if (url_regex.is_match(i))
                 {
                     for c in url_regex.captures_iter(i) {
-                        &self.url.push_str(&c[1]);
+                        &self.url.push_str(&c[1][0..c[1].len()-1]);
+                    }
+                }
+            if (version_regex.is_match(i))
+                {
+                    for c in version_regex.captures_iter(i) {
+
+                        &self.version.push_str(&c[1][0..c[1].len()-1]);
                     }
                 }
             if(file_name_regex.is_match(i)){
                 for c in file_name_regex.captures_iter(i) {
-                    &self.file_name.push_str(&c[1]);
+                    &self.file_name.push_str(&c[1][0..c[1].len()-1]);
                 }
             }
             if (build_regex.is_match(i)) {
@@ -64,58 +78,40 @@ impl Package {
             }
         }
     }
+
     fn install(&self) {
-        let mut current_dir =String::from(std::env::current_dir().unwrap().to_str().unwrap()) + &String::from("/");
+        println!("{}\n{}\n{}\n{}",self.build_instructions,self.file_name,self.version,self.url);
+        let mut  v : String = String::new();
+        let existing_check = checkers::file_already_installed(self.file_name.clone(),self.version.clone());
+        let version_check = checkers::new_version(self.file_name.clone(),self.version.clone());
+        if(existing_check){
+           return
+        }
+        if(version_check){
+            println!("You already have the newest version");
+            return
+        }
         let mut res = reqwest::get(&self.url).expect("Package not found");
-        let mut file = File::create(&mut self.file_name.clone()).expect("Error creating file");
+        let cl = &self.url.clone();
+        let x = cl.rfind("/").unwrap();
+        let mut file_name = &cl[x+1..cl.len()];
+        println!("{}",file_name);
+        let mut file = File::create(&mut file_name).expect("Error creating file");
         io::copy(&mut res, &mut file).expect("Error creating file");
-        let lines: &Vec<&str> = &self.build_instructions.split("&&").collect();
-        println!("{:?}",lines);
+        let mut lines: &Vec<&str> = &self.build_instructions.split("&&").collect();
         for x in 0..lines.len() {
-
+            let mut cmd = String::from(lines[x]);
+            cmd = self.set_env_vars(&mut cmd);
             if(&lines[x][1..3] == "cd"){
-
-                current_dir.push_str(&lines[x][4..lines[x].len()-1]);
-                current_dir.push_str("/");
-                continue;
-
-
-            }
-            if(current_dir != ""){
-                let file_name_regex = Regex::new(r"^(\w*).+").unwrap();
-                let mut file_name=  String::new();
-                if(file_name_regex.is_match(&lines[x][1..lines[x].len()])){
-                    for c in file_name_regex.captures_iter(&lines[x][1..lines[x].len()]){
-                        file_name.push_str(&c[1]);
-                    }
-
-                }
-                let file_path = String::from(current_dir.clone() + &file_name);
-                println!("{}",file_path);
-                match  fs::metadata(file_path.clone()){
-                    Ok(..) => {
-                        println!("FOUND IT");
-
-                        let o = current_dir.clone();
-
-                        let mut lo =  Command::new("sh").arg("-c").arg( "su -c \"".to_owned() + &current_dir.clone() + &lines[x][1..lines[x].len()-1] + &"\" - paradox".to_owned()).stdout(Stdio::piped()).stderr(Stdio::piped()).spawn().unwrap();
-                        let s = lo.stdout.as_mut().unwrap();
-                        let sr = BufReader::new(s);
-                        let sl = sr.lines();
-                        for li in sl{
-                            println!("{:?}",li);
-                        }
-                        continue;
-                    },
-                    Err(E) => {
-                    }
-
-                }
-
-
+                let mut current_dir =String::from(std::env::current_dir().unwrap().to_str().unwrap()) + &String::from("/");
+                current_dir.push_str(&cmd[4..cmd.len()-1]);
+                let path = Path::new(&current_dir);
+                std::env::set_current_dir(path);
 
             }
-            let mut nyan = Command::new("sh").arg("-c").arg(lines[x]).stdout(Stdio::piped()).stderr(Stdio::piped()).spawn().unwrap();
+
+            println!("{}",cmd);
+            let mut nyan = Command::new("sh").arg("-c").arg(cmd).stdout(Stdio::piped()).stderr(Stdio::piped()).spawn().unwrap();
             let s = nyan.stdout.as_mut().unwrap();
             let sr = BufReader::new(s);
             let sl = sr.lines();
@@ -123,6 +119,23 @@ impl Package {
                 println!("{:?}",li);
             }
         }
+        let mut package_checker = OpenOptions::new().append(true).read(true).write(true).open("/etc/parallax/package_list.txt").expect("Failed to read package_list");
+        package_checker.write_all((self.file_name.clone() + " " + &self.version).as_bytes());
+    }
+
+    fn set_env_vars(&self, line :&mut String) -> String{
+        let mut options = HashMap::new();
+        options.insert("$NAME",&self.file_name);
+        options.insert("$VERSION",&self.version);
+        let mut x = line.clone();
+        for (k,v) in options{
+            if(line.contains(k)){
+
+                x = x.replace(k,v);
+
+            }
+        }
+        x
     }
 }
 
@@ -130,32 +143,34 @@ fn main() {
     let mut Contents: String = String::new();
     let grab = App::new("Parallax").version("1.0").arg(
         Arg::with_name("install").long("install").takes_value(true)
-    ).get_matches();
+    ).arg(Arg::with_name("package-dir").long("package-dir").short("pd").takes_value(true)).get_matches();
+    let mut  d = "";
+    let dir = grab.value_of("package-dir");
 
-
+    match dir {
+        Some(dir) => { d = dir.clone() }
+        None => {}
+    }
     let pkg = grab.value_of("install");
-    match pkg{
-        Some(pkg) => {
 
-            let mut file = File::open(pkg.to_owned() + ".pkg");
-            match file{
-                Ok(mut file) =>
-                    {
-                        file.read_to_string(&mut Contents);
-                        let mut package: Package = Package {url: String::new(), build_instructions: String::new(), name : String::from(pkg), file_name: String::new()};
-                        package.set_vars(&Contents);
-                        println!("a");
-                        package.install();
-                    },
-                Err(file) => {
-                    println!("Package not found!");
-                    return
-                }
-            };
+        match pkg{
+        Some(pkg) => {
+            let mut package: Package = Package {url: String::new(), build_instructions: String::new(), name : String::from(pkg), file_name: String::new(), version: String::new()};
+            if(d == ""){
+                Contents = repo::load_url(pkg.clone().to_string());
+            }
+            else {
+                let mut file = File::open(d.to_owned() + &pkg.to_owned() + ".pkg").expect("fail");
+                file.read_to_string(&mut Contents);
+            }
+            package.set_vars(&Contents);
+            package.install();
+
 
         }
+
+
         None => println!("None")
     };
-
 
 }
